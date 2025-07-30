d1b0b16000ca1e8e02da2098ce26e95c393a3a0a
Ali <alihuseyni523@gmail.com>
2025-07-30 23:23:27 +0300

    Pair

---
+++ Stepik/Java.BasicСourse/Pair/src/main/java/com/myproject/Main.java
@@ -0,0 +1,61 @@
+package com.myproject;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+
+    class Pair<T, U> {
+        private final T first;
+        private final U second;
+
+        private Pair(T first, U second) {
+            this.first = first;
+            this.second = second;
+        }
+
+        public T getFirst() {
+            return first;
+        }
+
+        public U getSecond() {
+            return second;
+        }
+
+        public static <T, U> Pair<T, U> of(T first, U second) {
+            return new Pair<>(first, second);
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+
+            Pair<?, ?> other = (Pair<?, ?>) obj;
+
+            return objectsEqual(first, other.first) &&
+                    objectsEqual(second, other.second);
+        }
+
+        private boolean objectsEqual(Object a, Object b) {
+            return a == b || (a != null && a.equals(b));
+        }
+
+        @Override
+        public int hashCode() {
+            int result = first != null ? first.hashCode() : 0;
+            result = 31 * result + (second != null ? second.hashCode() : 0);
+            return result;
+        }
+    }
+}
